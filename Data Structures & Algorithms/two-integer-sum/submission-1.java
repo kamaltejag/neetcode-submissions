e3bb5880529ae63e443f49e1fc96402cd8f76130
Kamal Teja <packetcodeofficial@gmail.com>
2026-07-17 20:24:46 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complimentMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int compliment = target - nums[i];
+            if(complimentMap.containsKey(compliment)){
+                return new int[]{complimentMap.get(compliment), i};
+            }
+            complimentMap.put(nums[i], i);
+        }
+        return new int[2];
+    }
+}
